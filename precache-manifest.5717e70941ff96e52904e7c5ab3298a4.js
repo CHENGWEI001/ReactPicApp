2eb6aaf2502ef6ef2f8c3238a991e623ce1ba9cd
EC2 Default User <ec2-user@ip-172-31-37-125.us-west-2.compute.internal>
2018-12-27 23:38:18 +0000

    Updates

---
+++ precache-manifest.5717e70941ff96e52904e7c5ab3298a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "935e673d83c0a7449c9b",
+    "url": "/ReactPicApp/static/js/runtime~main.935e673d.js"
+  },
+  {
+    "revision": "d5122bc34c83ca35ec52",
+    "url": "/ReactPicApp/static/js/main.d5122bc3.chunk.js"
+  },
+  {
+    "revision": "02ba3405e7481ccc0a43",
+    "url": "/ReactPicApp/static/js/1.02ba3405.chunk.js"
+  },
+  {
+    "revision": "d5122bc34c83ca35ec52",
+    "url": "/ReactPicApp/static/css/main.da4b7df6.chunk.css"
+  },
+  {
+    "revision": "ba19e732d0fa1b3388dd5a324efae692",
+    "url": "/ReactPicApp/index.html"
+  }
+];
